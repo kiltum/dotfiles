64c30f7994c9555a1c5f14e9f3cd7884db2d6141
Viacheslav Kaloshin <kiltum@kiltum.tech>
2018-04-13 10:27:01 +0300

    add more files for backup

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 # Simple backup all private files from user directory
 cd
 now=$(date +"%d_%m_%Y")
-tar cvfz backup_${now}.tar.gz .ssh .gnupg
+tar cvfz backup_${now}.tar.gz .ssh .gnupg .aws .bash_history .config
