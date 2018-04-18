d752142db8d7a4a219748d778750b5f0344eed20
Viacheslav Kaloshin <kiltum@kiltum.tech>
2018-04-18 15:50:33 +0300

    add java remove better touc

--- brew.sh
+++ brew.sh
@@ -3,11 +3,6 @@
 sudo -v
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -60,7 +55,8 @@ brew install ansible
 #brew install packer
 brew install git-crypt
 
-brew install hetznercloud/tap/hcloud
+
+brew install hcloud
 brew install kubectl
 brew install kubernetes-helm
 
@@ -80,7 +76,8 @@ brew cask install telegram
 
 #brew cask install gcc-arm-embedded
 brew cask install highsierramediakeyenabler
-brew cask install bettertouchtool
+brew cask install java
+#brew cask install bettertouchtool
 
 # Remove outdated versions from the cellar.
 brew cleanup
