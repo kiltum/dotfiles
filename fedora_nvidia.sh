05869f9dd257d31fc2793ba84c620d6997e62d4b
Viacheslav Kaloshin <kiltum@kiltum.tech>
2019-09-24 15:40:18 +0300

    move to another repo

--- fedora_nvidia.sh
+++
@@ -1,5 +0,0 @@
-# its for gaming computer
-dnf install fedora-workstation-repositories
-dnf config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
-dnf config-manager --set-enabled rpmfusion-nonfree-steam
-dnf install xorg-x11-drv-nvidia akmod-nvidia
