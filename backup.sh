64d70b7524b8a1f41da36b32a15d717b67e5be91
Viacheslav Kaloshin <kiltum@kiltum.tech>
2020-10-29 11:00:27 +0300

    -- WIP ---

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 # Simple backup all private files from user directory
 cd
 now=$(date +"%d_%m_%Y")
-tar cvfz /Users/kiltum/Library/Mobile\ Documents/com\~apple\~CloudDocs/backup_${now}.tar.gz .ssh .gnupg .bash* .config crpt*
+tar cvfz /Users/kiltum/Library/Mobile\ Documents/com\~apple\~CloudDocs/backup_${now}.tar.gz .ssh .gnupg .bash* .config
