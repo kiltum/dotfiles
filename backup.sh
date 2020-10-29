f44aea479e456fd417be155b4bc5a61200ae3320
Viacheslav Kaloshin <kiltum@kiltum.tech>
2020-10-29 11:26:09 +0300

    backup 29_10_2020

--- backup.sh
+++ backup.sh
@@ -3,3 +3,7 @@
 cd
 now=$(date +"%d_%m_%Y")
 tar cvfz /Users/kiltum/Library/Mobile\ Documents/com\~apple\~CloudDocs/backup_${now}.tar.gz .ssh .gnupg .bash* .config .kube .krew
+cd /Users/kiltum/projects/dotfiles
+for i in $(ls -a | egrep '^\.'|egrep -v 'git$'|tail -n +3); do cp ~/${i} . ; done
+git commit -am "backup ${now}"
+git push
