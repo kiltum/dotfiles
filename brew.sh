2b8e2eec2b7561ed8df057356f494e46f1646709
Viacheslav Kaloshin <kiltum@kiltum.tech>
2021-08-23 15:23:32 +0300

    -- WIP ---

--- brew.sh
+++ brew.sh
@@ -13,7 +13,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
--- restore.sh
+++ restore.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 for i in $(ls -a | egrep '^\.'|egrep -v 'git$'|tail -n +3); do cp ${i} ~/ ; done
 LAST=$(ls -1 /Users/kiltum/Library/Mobile\ Documents/com\~apple\~CloudDocs/backup_*.tar.gz|sort -t _ -k 4,4 -k 3,3 -k 2,2| tail -1)
 cd ~
