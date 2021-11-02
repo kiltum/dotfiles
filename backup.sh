943e2cd2a641db869e5bee1790479eca398ae81b
Viacheslav Kaloshin <kiltum@kiltum.tech>
2021-11-02 10:43:39 +0300

    backup 02_11_2021

--- backup.sh
+++ backup.sh
@@ -3,14 +3,16 @@
 cd ~
 now=$(date +"%d_%m_%Y")
 wsl=$(set|grep WSL|wc -l)
-if [ $wsl > 0 ];
+if [ $wsl -eq "0" ];
 then
-    tar cvfz /mnt/c/Users/multi/OneDrive/backup_${now}.tar.gz .ssh .gnupg .bash* .config .kube .krew
-    cd /home/kiltum/projects/dotfiles
-else
+    echo "We are on OS X"
     tar cvfz /Users/kiltum/Library/Mobile\ Documents/com\~apple\~CloudDocs/backup_${now}.tar.gz .ssh .gnupg .bash* .config .kube .krew
     cd /Users/kiltum/projects/dotfiles
-
+    
+else
+    echo "We are on WSL"
+    tar cvfz /mnt/c/Users/multi/OneDrive/backup_${now}.tar.gz .ssh .gnupg .bash* .config .kube .krew
+    cd /home/kiltum/projects/dotfiles
 fi
 for i in $(ls -a | egrep '^\.'|egrep -v 'git$'|tail -n +3); do cp ~/${i} . ; done
 git commit -am "backup ${now}"
